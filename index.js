1c0ad48e603e7d62d09122f65c414013559208d9
a.dvoryan <a.dvoryan@gmail.com>
2024-10-28 22:58:35 +0100

    server update

---
+++ index.js
@@ -0,0 +1,45 @@
+import { createServer } from 'http';
+import process from 'node:process';
+
+let count = 0;
+
+console.log(process.env); 
+
+const server = createServer((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200);
+    res.end();
+    return;
+  }
+
+  res.write(`
+    <style>
+      body {
+        background-color: #1f1f1f;
+        color: white;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+    </style>
+    <h1>MEMES GEN</h1>`);
+    res.end();
+
+  if (req.url === '/api/memes') {
+    count+=1;
+    console.log(count);
+    
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify({ msg: count }));
+    res.end();
+  }
+});
+
+const PORT = 5000;
+server.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
